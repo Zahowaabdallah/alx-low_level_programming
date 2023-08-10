27046961bb156d7e95ad22ab406d3517533d9ef1
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-08-10 10:19:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A Program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+init main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
