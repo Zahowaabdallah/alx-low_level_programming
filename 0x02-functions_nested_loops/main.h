dd1c77eb1d94cd0743646393ad7b6cc19c6ae2cc
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-08-16 04:33:52 -0700

    abbsd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-end
+#endif
