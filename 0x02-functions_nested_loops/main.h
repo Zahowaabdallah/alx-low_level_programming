5d03f89132407e633d7e698ee21ce598d50c562a
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-08-15 19:09:26 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,18 @@
-#ifndef MAIN_HEADER_FILE
-#define MAI_HEADER_FILE
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-
-#endif
+/**
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    print_times_table(3);
+	        _putchar('\n');
+		    print_times_table(5);
+		        _putchar('\n');
+			    print_times_table(98);
+			        _putchar('\n');
+				    print_times_table(12);  
+				        return (0);
+}
