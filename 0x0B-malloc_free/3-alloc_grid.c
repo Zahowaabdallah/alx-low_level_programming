1f0ec8628087f816dcb6f51393b55deb6c451ec8
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-09-04 16:40:33 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 	int **tab, i, j;
 
-	tab = malloc(size(*tab) * height);
+	tab = malloc(sizeof(*tab) * height);
 
 	if (width <= 0 || height <= 0 || tab == 0)
 	{
