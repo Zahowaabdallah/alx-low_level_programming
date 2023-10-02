314302f55924bdc1ec4b383daa09d874dd3fa0a3
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-02 08:57:09 -0700

    a

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -9,4 +9,22 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i = 0, j;
+	char *p;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (*(accept + j) != '\0')
+		{
+			if (*(accept + j) == *(s + i))
+			{
+				p = s + i;
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
