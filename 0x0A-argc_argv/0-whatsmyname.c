dd5b118117eafa9672b6c3723c8272a99c89fa4a
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-02 10:48:55 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: int.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
