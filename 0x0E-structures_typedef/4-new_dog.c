1ef039bbfd516435f861fcca41d0da51fd452fdf
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-10 13:04:18 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,8 +23,8 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcpy - function
- * @dest: input
+ * _strcpy - function
+ * dest: input
  * @src: input
  * Return: pointer.
  */
@@ -43,48 +43,4 @@ char *_strcpy(char *dest, char *src)
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-	}
-	dest[i] = '\0';
 
-	return (dest);
-}
-
-/**
- * new_dog - function
- * @name: input
- * @age: input
- * @owner: input
- * Return: pointer.
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *dog;
-	int len1, len2;
-
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
-	{
-		free(dog);
-		free(dog->name);
-		return (NULL);
-	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
-
-	return (dog);
-}
