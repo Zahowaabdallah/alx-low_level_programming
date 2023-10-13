12dbbb796d06d620cf4947f3e97425d365cee3d0
Zahowaabdallah <139255498+Zahowaabdallah@users.noreply.github.com>
2023-10-14 01:22:45 +0300

    Update _printf.c

--- zahowa/_printf.c
+++ zahowa/_printf.c
@@ -1,1 +1,37 @@
-#include "main.h"}
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list list;
+    
+    va_start(list, format);
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                
+                _putchar(va_arg(list, int));
+            }
+            else if (*format == '%'){
+               
+                _putchar('%');
+            }
+            else if (*format == 's')
+            {
+               
+                print_string(va_arg(list, char *));
+            }
+            else {
+                
+            }
+        }
+        else{
+            _putchar(*format);
+        }
+        format++;
+    }
+}
