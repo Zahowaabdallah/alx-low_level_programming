b5bd63eecc898db30e23d3a5970511067b277682
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2023-10-26 08:50:28 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - func return value of bit in a given index
+ * @n: integer
+ * @index: index
+ * Return: index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
